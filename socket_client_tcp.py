e77b558b6c5749dec57fa362ff0335f228f4a896
FanLingJun <675546066@qq.com>
2019-09-16 13:29:25 +0800

    修改了若干问题

--- socket_client_tcp.py
+++ socket_client_tcp.py
@@ -3,78 +3,89 @@
 import socket
 import json
 
+s = None
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 12346
-
-
-try:
-  s.connect((host,port)) # 定义连接
-except Exception as e:
-  print("连接服务器失败",e)
-  exit(-1)
-
-
-while True:
+def init():
+  global s
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  host = socket.gethostname()
+  port = 12346
 
   try:
+    s.connect((host,port)) # 定义连接
+  except Exception as e:
+    print("连接服务器失败",e)
+    exit(-1)
 
-    # 发送登陆信息
-    msg = 'msg'
-    name = input("input name: ")
-    password = input("input password: ") # python 的getpass模块只能在指定ide中才起作用，故这里依然明文显示
-    client_info = json.dumps({'name':name, 'password':password}).encode('utf-8')
-    s.send(client_info)
+def login():
+  # 发送登陆信息
+  name = input("input name: ")
+  password = input("input password: ") # python 的getpass模块只能在指定ide中才起作用，故这里依然明文显示
+  client_info = json.dumps({'name':name, 'password':password}).encode('utf-8')
+  s.send(client_info)
 
-    msg = s.recv(1024).decode(encoding='utf-8') # 大坑！！！！！！！！！！！
-    # 只会接收一次，用msg去装载，如果
-    print(msg) # 接收TCP数据，数据以字符串形式返回，参数指定要接收的最大数据量(BUFSIZE)
+  msg = s.recv(1024).decode(encoding='utf-8') # 大坑！！！！！！！！！！！
+  # 只会接收一次，用msg去装载，如果先接受信息，再去赋值给msg那么就赋值失败了
+  # 接收TCP数据，数据以字符串形式返回，参数指定要接收的最大数据量(BUFSIZE)
+  print(msg)
 
-    if msg == 'name or password is incorrect!':
+  if msg == 'name or password is incorrect!':
+    exit()
+
+  s.sendall('Connected!'.encode(encoding='utf-8'))
+
+
+def get_message():
+
+  while True: # 不断接收消息
+    data = s.recv(1024).decode(encoding='utf-8')
+    if data == 'close':
+      print('The server closed your client!')
+      s.close()
       exit()
+    if not data:
+      break
+    print("\nA message from server:", end=' ')
+    print(data)
+    break
 
-    print('''
-                  |----------------------------------|
-                  | input 1:Sending message to server|
-                  | input 2:Close client             |
-                  | input 3:Client hanging           |
-                  |----------------------------------|
-      ''')
 
-    while True:
 
-      n = input()
+if __name__ == '__main__':
 
-      if n == '1':
-        message = input("input you message here: ")
-        s.sendall(message.encode(encoding='utf-8'))
-      elif n == '2':
-        print("The connection is closed")
+  init()
+  login()
+
+  print('''
+                |----------------------------------|
+                | input 1:Waiting for message...   |
+                | input 2:Sending message to client|
+                | input 3:Close server             |
+                |----------------------------------|
+    ''')
+
+  while True:
+    n = input(">>>")
+    if n == '1':
+      get_message()
+    elif n == '2': # 发送exit可自动退出
+      message = input("input you message here: ")
+      s.sendall(message.encode(encoding='utf-8'))
+      data = s.recv(1024).decode(encoding='utf-8')
+      if data == 'close':
+        print('The server closed your client!')
         s.close()
         exit()
-      elif n == '3':
-        # 并不立刻关闭，一直挂起客户端等待服务端发送信息回来
-        while True:
-
-          if not s.recv(1024).decode(encoding='utf8'):
-            print("The server closed you client")
-            exit()
-
-          print("A message from server:",end=' ')
-          print(s.recv(1024).decode(encoding='utf8'))
-
-  except Exception as e:
-    print("Some mistakes: ", e)
-    if not s.recv(1024).decode(encoding='utf8'):
-      print("The server closed you client")
+    elif n == '3':
+      print("The connection is closed")
+      s.close()
       exit()
 
 
-
-
-# input("")
-
 # 待解决的问题： 当服务器关闭时客户端主动关闭
+# 针对上面一个问题，即服务器关闭了而客户端不能
+# 立即接收到，只能等到客服端主动发起动作的时候才能收到关闭的信息
+# 解决：利用心跳包检测
+#
 # 客户端发送消息关闭
 # 关闭客户端时打印文字，正常关闭
--- socket_server_tcp.py
+++ socket_server_tcp.py
@@ -46,7 +46,7 @@ def accept_client():
       t.setDaemon(True)
       t.start()
     except Exception as e:
-      print("Some mistakes: ", e)
+      print("\nSome mistakes: ", e)
 
 
 def handle_message(client,address):
@@ -54,46 +54,41 @@ def handle_message(client,address):
   try:
 
     ret = client.recv(1024)
-    # print(ret)
-    # client_info = json.load(ret)
     client_info = json.loads(ret.decode(encoding='utf-8'))
-
     success = False
-    for i_info in info_list:
 
+    for i_info in info_list:
       name = i_info['name']
       password = i_info['password']
-
       if (name == client_info['name'] and str(password) == client_info['password']):
-        # print("success!")
         success = True
         break
 
     if success:
       client.sendall("Connected Successfully!".encode(encoding='utf8'))
+      data = client.recv(1024)
+      print("\nThe client host: ", address, end='')
+      print(" Client message: ", data.decode(encoding='utf-8'))
+
       while True:
         data = client.recv(1024)
-
-        if len(data) == 0 or data == 'exit':
-          client.close()
-          # 删除连接
+        if len(data) == 0 or data == b'exit':
+          msg = 'close'
+          client.sendall(msg.encode(encoding='utf8'))
+          client.close() # 删除连接
           connection_pool.remove(client)
-          print("One client closed.")
+          print("\nOne client closed.")
           break
-
-        print("The client host: ", address,end='')
-        print(" Client message: ", data)
-
+        client.sendall('get message'.encode(encoding='utf8'))
+        print("\nThe client host: ", address,end='')
+        print(" Client message: ", data.decode(encoding='utf-8'))
     else:
       client.sendall("name or password is incorrect!".encode('utf-8'))
-
       client.close()
-
       connection_pool.remove(client)
 
   except Exception as e:
-    print("Some mistakes: ",e)
-
+    print("\nSome mistakes: ",e)
 
 if __name__ == '__main__':
 
@@ -107,13 +102,13 @@ if __name__ == '__main__':
               | input 1:Checking online numbers  |
               | input 2:Sending message to client|
               | input 3:Close server             |
-              | input 4:Close one client         |
+              | input 4:Close client             |
               |----------------------------------|
   ''')
 
   while True:
 
-    n = input()
+    n = input(">>>")
 
     if n == '1':
       print("Online number is : ",len(connection_pool))
@@ -123,14 +118,16 @@ if __name__ == '__main__':
       connection_pool[int(index)].sendall(msg.encode(encoding='utf8'))
 
     elif n == '3':
+      s.close()
       exit()
+
     elif n == '4':
       close_i = input("Please input the close index ")
+      msg = 'close'
+      connection_pool[int(close_i)].sendall(msg.encode(encoding='utf8'))
       connection_pool[int(close_i)].close()
-
-
-# 待解决问题
-# 添加异常处理
+      # connection_pool[int(close_i)].remove()
+      # 还没有解决
 
 
 
