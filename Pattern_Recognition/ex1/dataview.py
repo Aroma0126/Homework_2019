d470f9dc80056d9a0d6fb366fb55f93bd285d5fd
FanLingJun <675546066@qq.com>
2019-11-30 15:56:59 +0800

    add to do

---
+++ Pattern_Recognition/ex1/dataview.py
@@ -0,0 +1,25 @@
+
+'''
+ex1 可视化数据集1/2的数据
+思路 先读取数据存到list里面 然后利用 matplotlib
+dataset1 infos of student 从excel里读出
+
+属性
+1.height in cm
+2.weight in kg
+3.size of feet in 中国鞋码
+
+iris.dat
+属性
+1. sepal length in cm
+2. sepal width in cm
+3. petal length in cm
+4. petal width in cm
+5. class:
+	1-- Iris Setosa
+	2-- Iris Versicolour
+	3-- Iris Virginica
+
+'''
+
+import pyecharts
